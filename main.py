7eebff91e739210f77a0b66b6fc3832b9a82ca22
jaymody <jaykmody@gmail.com>
2021-02-15 11:25:46 +0000

    set seed to allow deterministic evaluation scores

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import pickle
 import argparse
 
 from train import evaluate, load_model
-from utils import load_file
+from utils import load_file, set_seed
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("dirpath", type=str)
     parser.add_argument("--model_ckpt", type=str, default="model.ckpt")
     parser.add_argument("--data_path", type=str, default="data/test_set.txt")
+    parser.add_argument("--seed", type=int, default=1234)
     args = parser.parse_args()
 
+    set_seed(args.seed)
     pairs = load_file(args.data_path)
     model = load_model(args.dirpath, args.model_ckpt)
     evaluate(model, pairs)
